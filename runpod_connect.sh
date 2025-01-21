6551eea5cc460df7f01697834cedba2dbded7148
René Fa <farene1996+github@gmail.com>
2025-01-21 15:18:08 +0100

    Add rsync and runpod connect and sync command

---
+++ runpod_connect.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+pushd $(dirname $0)
+
+echo "Get endpoint..."
+ENDPOINT=$(runpodctl get pod -a | cut -f12 | sed -rn '2{s/^.*,([0-9.:]+)->22.*/\1/p}')
+
+IP=$(echo $ENDPOINT | cut -d':' -f 1)
+PORT=$(echo $ENDPOINT | cut -d':' -f 2)
+
+echo $IP $PORT
+
+echo "Upload userdata..."
+rsync -avz -e "ssh -p $PORT" --delete "comfyui_user/" "root@$IP:/data/comfyui_user"
+
+echo "Open shell..."
+ssh root@$IP -p $PORT -L 8188:localhost:8188
+
+echo "Download userdata..."
+rsync -avz -e "ssh -p $PORT" --delete root@$IP:/data/comfyui_user/ comfyui_user/
